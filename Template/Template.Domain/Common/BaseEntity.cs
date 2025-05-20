07f2c495443a707afb5f8af22a90f4f083163cec
Hosseini Seyed Mohammad <M_Hosseini@iranet.net>
2025-05-20 14:40:26 +0330

    c

---
+++ Template/Template.Domain/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Domain.Common
+{
+    public class BaseEntity
+    {
+        public Guid Key { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+
+    public interface IBaseEntity
+    { 
+    }
+}
